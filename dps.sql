1e7c7785f08f2a3025e2fa8d1d74074cb9b68a20
Divyansh Pratap Singh <104272009+dev-divyansh@users.noreply.github.com>
2022-06-29 03:35:00 +0530

    created dps.sql
    this file consist of code for mysql database : just import the file in mysql

---
+++ dps.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE gov;
+
+USE gov;
+
+CREATE TABLE DPS(year int(50) , april varbinary(5000) , may varbinary(5000), june varbinary(5000)
+ , july varbinary(5000), august varbinary(5000), september  varbinary(5000)
+, october varbinary(5000), november varbinary(5000) , december varbinary(5000)
+, january varbinary(5000), february varbinary(5000), march varbinary(5000));
